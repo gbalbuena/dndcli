f840491751f558251edd33716e8afd69cd878a41
Juan Gabriel Balbuena Borba <gabrielbalbuena@gmail.com>
2019-07-05 16:18:01 +1000

    cli implementation using dnd, use npm link

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # Developers and Ducks library
 
-## Bugs
+## Local
 
-`characterService#getModifier`: has a wrong interpretation when `modifier` is negative
-Clue maybe yaml is not parsing correctly
+```bash
+npm link
+```
+
+```bash
+create-character
+```
+
+## Cli
+
+Not implemented yet
+
+### Initiative
+
+```bash
+dnd initiative --player='Good Paladin' --player=evil-wizard.yaml --creature='Imp' --creature=final-monster.json
+```
+
+### Character
+
+```bash
+dnd character create
+```
+
+### Creature
+
+```bash
+dnd creature create
+```
+
+### Equipment
+
+```bash
+dnd equipment create
+```
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,28 @@
-export function cli(args) {
-  console.log(args);
-}
+import inquirer from 'inquirer';
+
+async function prompt() {
+  const questions = [];
+    questions.push({
+      type: 'input',
+      name: 'name',
+      message: 'Name of your adventurer?',
+      default: 'Unknown Adventurer'
+    });
+
+    questions.push({
+      type: 'list',
+      name: 'race',
+      message: 'Please choose which race would you like to be?',
+      choices: ['Human', 'Developer'],
+      default: 'Human',
+    });
+
+
+  const character = await inquirer.prompt(questions);
+  return character;
+ }
+
+export async function cli(args) {
+  let options = await prompt();
+  console.log(options);
+ }
