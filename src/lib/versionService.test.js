412a4a0c86abcb4890becf5d9594a9e16cdb4209
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2019-08-19 18:18:39 +1000

    Fix test

--- src/lib/versionService.test.js
+++ src/lib/versionService.test.js
@@ -2,5 +2,5 @@ import pachage from '../../package.json';
 import versionService from './versionService';
 
 test('get version', () => {
-  expect(versionService.getVersion()).toEqual(`${pachage.name}: v${pachage.version}`);
+  expect(versionService.getVersion()).toInclude(pachage.name, pachage.version);
 });
