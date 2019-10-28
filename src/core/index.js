42f25d5b9acb03e18c008cb05a328e956302ed4c
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2019-10-28 12:26:16 +1100

    ac: add test and finalize function

--- src/core/index.js
+++ src/core/index.js
@@ -1,16 +1,5 @@
 import datastore from '../datastore/index';
 
-const base_ac = {
-  no_armor: 10,
-  leather_armour: 11,
-  chain_shirt: 13,
-  plate_mail: 18,
-  mage_armour_spell: 13,
-  barbarian_unarmoured_defense_ability: 10,
-  monk_unarmoured_defense_ability: 10,
-  sorcerer_draconic_resilience_ability: 13
-}
-
 function ac({
   armorId = 'no_armor',
   dexterity_modifier = 0,
@@ -22,19 +11,26 @@ function ac({
   const armorItem = datastore.armor.find((armor) => armor.id === armorId);
 
   if (armorItem.type === 'Medium Armor' && dexterity_modifier > 2) {
-    dexterity_modifier = 2;
+    dexterity_modifier = 2; // max dex = 2
   }
 
   if (armorItem.type === 'Heavy Armor') {
     dexterity_modifier = 0; // ignore dex
   }
 
-  let ac_total = armorItem.ac + dexterity_modifier;
+  let ac_total = armorItem.ac;
 
-        // let ac_total = base_ac[armor] +
-        //   ((armor === 'plate_mail') ? 0 : dexterity_modifier) +
-        //   ((armor === 'barbarian_unarmoured_defense_ability') ? dexterity_modifier + constitution_modifier: 0) +
-        //   ((armor === 'monk_unarmoured_defense_ability') ? dexterity_modifier + wisdom_modifier : 0);
+  armorItem.modifier.forEach((mod) => {
+    if (mod === 'dexterity') {
+      ac_total = ac_total + dexterity_modifier;
+    }
+    if (mod === 'constitution') {
+      ac_total = ac_total + constitution_modifier;
+    }
+    if (mod === 'wisdom') {
+      ac_total = ac_total + wisdom_modifier;
+    }
+  });
 
   if (half_cover) {
     ac_total = ac_total + 2;
--- src/core/index.test.js
+++ src/core/index.test.js
@@ -53,4 +53,58 @@ describe('ac', () => {
       expect(dnd.ac({ armorId: 'plate_mail', dexterity_modifier: 1, shield: true })).toEqual(20);
     });
   });
+
+  describe('spell', () => {
+    test('mage armour spell', () => {
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: -1 })).toEqual(12);
+
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: 0 })).toEqual(13);
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: 1 })).toEqual(14);
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: 2 })).toEqual(15);
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: 3 })).toEqual(16);
+
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: 1, half_cover: true })).toEqual(16);
+      expect(dnd.ac({ armorId: 'mage_armour_spell', dexterity_modifier: 1, shield: true })).toEqual(16);
+    })
+  });
+  describe('defense ability', () => {
+    test('barbarian unarmoured', () => {
+      const armorId = 'barbarian_unarmoured';
+      expect(dnd.ac({ armorId, dexterity_modifier: -1, constitution_modifier: -1 })).toEqual(8);
+
+      expect(dnd.ac({ armorId, dexterity_modifier: 0, constitution_modifier: 0 })).toEqual(10);
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, constitution_modifier: 1 })).toEqual(12);
+      expect(dnd.ac({ armorId, dexterity_modifier: 2, constitution_modifier: 2 })).toEqual(14);
+      expect(dnd.ac({ armorId, dexterity_modifier: 3, constitution_modifier: 3 })).toEqual(16);
+
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, constitution_modifier: 1, half_cover: true })).toEqual(14);
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, constitution_modifier: 1, shield: true })).toEqual(14);
+    });
+    test('monk unarmoured', () => {
+      const armorId = 'monk_unarmoured';
+      expect(dnd.ac({ armorId, dexterity_modifier: -1, wisdom_modifier: -1 })).toEqual(8);
+
+      expect(dnd.ac({ armorId, dexterity_modifier: 0, wisdom_modifier: 0 })).toEqual(10);
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, wisdom_modifier: 1 })).toEqual(12);
+      expect(dnd.ac({ armorId, dexterity_modifier: 2, wisdom_modifier: 2 })).toEqual(14);
+      expect(dnd.ac({ armorId, dexterity_modifier: 3, wisdom_modifier: 3 })).toEqual(16);
+
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, wisdom_modifier: 1, half_cover: true })).toEqual(14);
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, wisdom_modifier: 1, shield: true })).toEqual(14);
+    });
+  });
+  describe('resilience ability', () => {
+    test('sorcerer draconic', () => {
+      const armorId = 'sorcerer_draconic';
+      expect(dnd.ac({ armorId, dexterity_modifier: -1 })).toEqual(12);
+
+      expect(dnd.ac({ armorId, dexterity_modifier: 0 })).toEqual(13);
+      expect(dnd.ac({ armorId, dexterity_modifier: 1 })).toEqual(14);
+      expect(dnd.ac({ armorId, dexterity_modifier: 2 })).toEqual(15);
+      expect(dnd.ac({ armorId, dexterity_modifier: 3 })).toEqual(16);
+
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, half_cover: true })).toEqual(16);
+      expect(dnd.ac({ armorId, dexterity_modifier: 1, shield: true })).toEqual(16);
+    });
+  });
 })
