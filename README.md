6b338c79d1a4e1211018d2f990c3db68aa7aa52e
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2019-10-28 13:47:03 +1100

    ac: update armor data

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
-# Developers and Ducks library
+# opendnd5e
 
-## Data structure and relations
+## setup
 
 ```txt
-[Player] 1...n [Characters]
-```
-
-## Usage
-
-Search classes:
-
-```bash
-dnd classes | jq '.[] |= del(.desc,.table,.archetypes,.document_slug) | map(select(.name | contains("Wizard")))'
-```
-
-Search weapons by name:
-
-```bash
-dnd weapons | jq 'map(select(.name | contains("Long")))'
-dnd weapons | jq 'map(select(.category | contains("Martial")))'
+npm i
+npm link
+dnd
 ```
--- src/cli.js
+++ src/cli.js
@@ -1,28 +1,26 @@
-import program from 'commander';
-import package from '../package.json';
+import commander from 'commander';
+import Table from 'terminal-table';
+
 
 import datastore from './datastore';
-import { format, source } from './config';
+// import { format, source } from './config';
 
 export async function cli(args) {
-  console.log(version.getVersion());
-  program
-    .version(package.version)
-    .option('-d, --debug', 'output extra debugging')
+  const program = new commander.Command();
+  program.version('0.0.1');
 
   program
-    .command('config')
-    .alias('conf')
-    .description('show configuration')
-    .action((opts) => {
-      console.log({  format: format(), source: source() });
-    });
+    .option('-d, --debug', 'output extra debugging')
 
   program
     .command('armor')
     .description('show armor')
-    .action((name, opts) => {
-      console.log(datastore.armor);
+    .action(() => {
+      var t = new Table();
+      datastore.armor.forEach((item) => {
+        t.push([item.id, item.name, item.type, item.ac, ]);
+      });
+      console.log('' + t)
     });
 
   program.parse(args);
