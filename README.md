a34b3cdf6d894f2a0b6c75ffcba8f3f452b63226
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2019-10-28 14:01:29 +1100

    armor: update cli cmd

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ npm i
 npm link
 dnd
 ```
+
+## Armor
+
+```bash
+$ dnd armor
+table...
+```
+
+```bash
+$ dnd armor --format=json | jq -r .[].name
+No Armor
+Padded Armor
+Leather Armor
+...
+```
--- src/cli.js
+++ src/cli.js
@@ -11,15 +11,23 @@ export async function cli(args) {
 
   program
     .option('-d, --debug', 'output extra debugging')
+    .option('-f, --format <format>', 'format')
 
   program
     .command('armor')
     .description('show armor')
     .action(() => {
+      if (program.format === 'json') {
+        console.log(JSON.stringify(datastore.armor));
+        return;
+      }
+
       var t = new Table();
+      t.push(['Name', 'Type', 'A.C.', 'Cost', 'weight', 'Disadvantage']);
       datastore.armor.forEach((item) => {
-        t.push([item.id, item.name, item.type, item.ac, ]);
+        t.push([item.name, item.type, item.ac, item.cost, item.weight, item.disadvantage]);
       });
+      t.attrRange({ row: [0, 1] }, { align: "center" });
       console.log('' + t)
     });
 
